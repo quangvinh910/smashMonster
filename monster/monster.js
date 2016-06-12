cf164804cf0fd16583513c1504f5596d0c97b6d4
quangvinh910 <quangvinh910@gmail.com>
2016-06-12 13:46:54 +0700

    push the new stuff

---
+++ monster/monster.js
@@ -0,0 +1,7 @@
+/**
+ * Created by USER on 6/12/2016.
+ */
+function Monster(){
+    this.skill = [];
+    this.sprite;
+}
---
+++ monster/skill/Skill.js
@@ -0,0 +1,8 @@
+/**
+ * Created by USER on 6/12/2016.
+ */
+function Skill(){
+    this.slowPercent;
+    this.damagePercent;
+    this.sprite;
+}
